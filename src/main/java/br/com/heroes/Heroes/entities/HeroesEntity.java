1527ab894c666e2a54cc961539297f48132b9042
Archimedes Fagundes Junior <archimedes.junior@nava.com.br>
2022-08-03 18:04:30 -0300

    ajuste na entidade do herói para que o id seja gerado sequencialmente pelo banco de dados.

--- src/main/java/br/com/heroes/Heroes/entities/HeroesEntity.java
+++ src/main/java/br/com/heroes/Heroes/entities/HeroesEntity.java
@@ -23,7 +23,7 @@ public class HeroesEntity {
 	}
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public Integer getId() {
 		return id;
 	}
